813dc1e9243fd15a4d5bddd2ee2f4dbddfb49c92
joesati18 <119226562+joesati18@users.noreply.github.com>
2023-05-15 16:46:40 +0100

    Add files via upload

---
+++ CMP114assignment.py
@@ -0,0 +1,8 @@
+#program to print even numbers between 4 and 30
+
+even_numbers = [num for num in range(4, 30) if num % 2 == 0]
+
+print(even_numbers)
+
+# Name: Sati Joseph Victor
+# Matric No.: BHU/22/04/05/0106
